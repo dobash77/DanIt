75ee2240fcc3cfd9200c8fd771d2203f7e849d24
Dobash <dobash77@gmail.com>
2023-03-10 13:06:21 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int i = 100;
+        char ch = 122;
+        System.out.println(ch+i);
+        System.out.println(ch);
+
+    }
+}
