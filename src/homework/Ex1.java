05f4ccf55a934513c509b591b2df72922a92fa09
Dobash <dobash77@gmail.com>
2023-03-13 20:12:30 +0300

    add ex1

--- src/homework/Ex1.java
+++ src/homework/Ex1.java
@@ -2,11 +2,12 @@ package homework;
 
 public class Ex1 {
     public static void main(String[] args) {
-        int x = 3;
+       int x = 3;
         int y = ++x*2;
         x+=-y*3 + ++x + --y;
         y+=(-x++)%3;
         int z = x + y;
-        System.out.println(x+" " +y+ " "+z);;
+        System.out.println(x+" " +y+ " "+z);
+
     }
 }
