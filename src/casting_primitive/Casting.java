7f0ca555dcd638111afe7efdfb5e8ee8c0a7f667
Dobash <dobash77@gmail.com>
2023-03-22 08:22:37 +0300

    add file casting

---
+++ src/casting_primitive/Casting.java
@@ -0,0 +1,26 @@
+package casting_primitive;
+
+public class Casting {
+    public static void main(String[] args) {
+        /*byte a = 100;
+        a = (byte) (a*2);
+        System.out.println(a);*/
+
+
+        /*char ch = 'A';
+        System.out.println(ch);
+        char ch1 = 111;
+        System.out.println(ch1);*/
+
+        int a = 100;
+        char ch = 122;
+        System.out.println(ch);
+        System.out.println(a+ch);
+
+
+        /*double b = 63;
+        double c = 13;
+        int d = (int) (b/c);
+        System.out.println(d);*/
+    }
+}
